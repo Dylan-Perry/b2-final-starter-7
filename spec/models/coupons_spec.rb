b070347bf42d83f85262ec7631dea2bfc0cee906
Dylan Perry <dylanbperry@gmail.com>
2024-01-15 11:01:53 -0800

    Fix tests to reflect coupon code uniqueness across entire database, instead of singular merchant

--- spec/models/coupons_spec.rb
+++ spec/models/coupons_spec.rb
@@ -4,21 +4,21 @@ describe Coupon do
   describe "validations" do
     it { should validate_presence_of :name }
     it { should validate_presence_of :coupon_code }
-    it { should validate_uniqueness_of(:coupon_code).scoped_to(:merchant_id) }
+    it { should validate_uniqueness_of :coupon_code }
     it { should validate_presence_of :discount_amount }
     it { should validate_presence_of :discount_type }
     it { should validate_presence_of :status }
     it { should validate_presence_of :merchant_id }
   end
-
+  
+  describe "relationships" do
+    it { should belong_to :merchant }
+    it { should have_many :invoices }
+  end
+  
   describe "enums" do
     it { should define_enum_for(:discount_type).with_values([:dollars, :percent]) }
     it { should define_enum_for(:status).with_values([:active, :inactive]) }
-  end
-
-  describe "relationships" do
-    it { should belong_to :merchant }
-    it { should belong_to(:invoice).optional }
   end
 
   describe "defaults" do
@@ -35,6 +35,6 @@ describe Coupon do
     coupon = create(:coupon, coupon_code: "BOGO14", merchant_id: merchant1.id)
 
     expect{ Coupon.create!(name: "Coupon", coupon_code: "BOGO14", discount_amount: 5, discount_type: 1, merchant_id: merchant1.id) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Coupon code has already been taken")
-    expect{ create(:coupon, coupon_code: "BOGO14", merchant_id: merchant2.id) }.to_not raise_error
+    expect{ Coupon.create!(name: "Coupon", coupon_code: "BOGO14", discount_amount: 5, discount_type: 1, merchant_id: merchant2.id) }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Coupon code has already been taken")
   end
 end
